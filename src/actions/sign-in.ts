6023ee669a6e5b3c20772a85a9a0ff623c7505a9
NicolasMouret <mouret.nicolas63@gmail.com>
2023-12-31 16:30:28 +0100

    try fix auth on vercel

--- src/actions/sign-in.ts
+++ src/actions/sign-in.ts
@@ -1,13 +1,14 @@
 'use server';
 
 import * as auth from '@/auth';
+import { signIn } from 'next-auth/react';
 
 export async function signInGithub() {
   return auth.signIn('github');
 }
 
 export async function signInGoogle() {
-  return auth.signIn('google', { callbackUrl: process.env.NEXTAUTH_URL });
+  return signIn('google');
 }
 
 export async function signInChoice() {
