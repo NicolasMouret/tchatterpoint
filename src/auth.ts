37e03ec9443c0cc220e5360f85074d0bcd12d8a4
NicolasMouret <mouret.nicolas63@gmail.com>
2024-01-04 22:09:59 +0100

    Revert "force dynamic on cantina page"
    This reverts commit df1886363b34921fbb1fcaf2acdca414067f2969.

--- src/auth.ts
+++ src/auth.ts
@@ -93,7 +93,6 @@ export const {
     strategy: 'jwt',
   },
   secret: process.env.NEXTAUTH_SECRET,
-  trustHost: true,
   callbacks: {
     // Usually not needed, here we are fixing a bug in nextauth
     async session({ session, user, token }: any) {
