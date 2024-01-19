394a0c1cb972accc6479b8ef3dc1ec154e31db02
NicolasMouret <mouret.nicolas63@gmail.com>
2024-01-20 00:07:41 +0100

    try fix: env variable for pusher

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -6,21 +6,21 @@ const PUSHER_KEY = process.env.PUSHER_KEY
 const PUSHER_SECRET = process.env.PUSHER_SECRET
 const PUSHER_CLUSTER = process.env.PUSHER_CLUSTER
 
-if (!PUSHER_APP_ID
-  || !PUSHER_KEY
-  || !PUSHER_SECRET
-  || !PUSHER_CLUSTER) {
-  throw new Error('Missing Pusher env variables')
-}
+// if (!PUSHER_APP_ID
+//   || !PUSHER_KEY
+//   || !PUSHER_SECRET
+//   || !PUSHER_CLUSTER) {
+//   throw new Error('Missing Pusher env variables')
+// }
 
 export const pusherServer = new PusherServer({
-  appId: PUSHER_APP_ID,
-  key: PUSHER_KEY,
-  secret: PUSHER_SECRET,
-  cluster: PUSHER_CLUSTER,
+  appId: PUSHER_APP_ID!,
+  key: PUSHER_KEY!,
+  secret: PUSHER_SECRET!,
+  cluster: PUSHER_CLUSTER!,
   useTLS: true,
 });
 
-export const pusherClient = new PusherClient(PUSHER_KEY, {
-  cluster: PUSHER_CLUSTER,
+export const pusherClient = new PusherClient(PUSHER_KEY!, {
+  cluster: PUSHER_CLUSTER!,
 });
