e5294413cbe06abdb86d57dd151874b5c8c5add2
NicolasMouret <mouret.nicolas63@gmail.com>
2024-01-23 16:42:55 +0100

    refacto: switch realtime handling from pusher to supabase

--- src/app/messages/[slug]/[chatId]/page.tsx
+++ src/app/messages/[slug]/[chatId]/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from '@/auth';
 import ConversationShow from '@/components/messages/conversation-container';
 import ChatInputForm from '@/components/messages/conversation-input';
+import PresenceIndicator from '@/components/messages/presence-indicator';
 import { ChatComplete, fetchChatComplete } from '@/db/queries/chats';
 import { Avatar, Card, Divider, Link } from "@nextui-org/react";
 
@@ -22,6 +23,9 @@ const isUserInChat = (chat: ChatComplete, userId: string) => {
 
 export default async function ChatPage({ params }: ChatPageProps) {
   const session = await auth();
+  const chatId = params.chatId;
+  const interlocutorName = decodeURIComponent(params.slug);
+
   if (!session) {
     return (
       <Card isBlurred className="flex flex-col items-center gap-4 p-3 w-full sm:w-4/5">
@@ -29,9 +33,7 @@ export default async function ChatPage({ params }: ChatPageProps) {
       </Card>
     )
   }
-
   const {name: userName, id: userId} = session.user;
-  const chatId = params.chatId;
 
   const chat = await fetchChatComplete(chatId);
   if (!chat) {
@@ -49,8 +51,8 @@ export default async function ChatPage({ params }: ChatPageProps) {
       </Card>
     )
   }
-  const interlocutorName = decodeURIComponent(params.slug);
   const interlocutorImage = getInterlocutorImage(chat, interlocutorName);
+
   
   return (
     <section className="flex flex-col items-center p-2 w-[95%] sm:w-4/5 h-[88vh] mb-2
@@ -64,10 +66,13 @@ export default async function ChatPage({ params }: ChatPageProps) {
           radius="md"
           src={interlocutorImage}/>
         <span className="font-bold text-yellow-400 text-xl">Conversation avec {interlocutorName}</span>
+        <PresenceIndicator 
+          interlocutorName={interlocutorName}
+          chatId={chatId}/>
       </div>
       <Divider/>
       <ConversationShow 
-        InitialMessages={chat.messages} 
+        initialMessages={chat.messages} 
         userName={userName}
         chatId={chatId}/>
       <Divider/>
