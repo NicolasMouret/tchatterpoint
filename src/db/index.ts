a2fc12ea45dfb06be4c0f3c73b037c3035249a4e
NicolasMouret <mouret.nicolas63@gmail.com>
2024-01-23 16:47:35 +0100

    fix: supabase env variables

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 import { createClient } from '@supabase/supabase-js';
 
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-  throw new Error('Missing Supabase env variables')
-}
+
 
 export const db = new PrismaClient();
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 )
