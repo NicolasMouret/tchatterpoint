4bc66196f60318a005273af5c1a389bb5ff5d071
NicolasMouret <mouret.nicolas63@gmail.com>
2024-01-25 17:11:49 +0100

    fix: chat realtime refresh

--- src/components/messages/chat-list.tsx
+++ src/components/messages/chat-list.tsx
@@ -1,20 +1,22 @@
 import { auth } from '@/auth';
-import { fetchChatsForList } from "@/db/queries/chats";
+import { ChatForList, fetchChatsForList, hasUnreadMessages } from "@/db/queries/chats";
 import { Avatar, Card } from '@nextui-org/react';
 import Link from 'next/link';
 import ChatUnreadCount from './chat-unread-count';
 
 
-
 export default async function ChatList() {
   const session = await auth();
-  if (!session) {
+  if (!session || !session.user) {
     return (
       <div>Vous devez être connecté pour accéder à la messagerie</div>
     )
   }
+
   const chats = await fetchChatsForList(session.user.id);
-  const renderedChats = chats.map((chat) => {
+  const chatCard = async (chat: ChatForList) => {
+    const hasUnread = await hasUnreadMessages(chat.id, session.user.id);
+    console.log("hasUnread", hasUnread);
     return (
       <Link key={chat.id} 
         className="w-[95%] sm:w-3/5"
@@ -30,16 +32,16 @@ export default async function ChatList() {
               {chat.lastMessage && 
               <span className="text-xs text-slate-400">{chat.lastMessage.content.slice(0, 30)}...</span>}
             </div>
-            <ChatUnreadCount chatId={chat.id}/>
+            <ChatUnreadCount initialUnreadState={hasUnread} chatId={chat.id} />
         </Card> 
       </Link>
     )
-  })
+  }
   
   return (
     <>
       <h1 className="font-bold text-2xl text-yellow-400">Conversations</h1>
-      {renderedChats}
+      {chats.map(chatCard)}
     </>
   )
   
--- src/components/messages/chat-unread-count.tsx
+++ src/components/messages/chat-unread-count.tsx
@@ -1,34 +1,50 @@
-import { auth } from "@/auth";
+'use client';
+import { supabase } from "@/db";
 import { Chip } from "@nextui-org/chip";
+import { useEffect, useState } from "react";
 
 interface ChatUnreadCountProps {
   chatId: string;
+  initialUnreadState?: boolean;
 }
 
-export default async function ChatUnreadCount({ chatId }: ChatUnreadCountProps) {
-  const session = await auth();
-  if (!session) {
-    return null;
-  }
-  const unreadCount = session.user.unreadMessages?.find(
-    (unread) => unread.chatId === chatId
-  )?.count || 0;
-  if (unreadCount === 0 || unreadCount === null) {
-    return null;
-  }
+export default function ChatUnreadCount({ chatId, initialUnreadState }: ChatUnreadCountProps) {
+  const [hasUnread, setHasUnread] = useState(initialUnreadState);
+  useEffect(() => {
+    setHasUnread(initialUnreadState);
+    console.log("initialUnreadState", initialUnreadState);
+    const channel = supabase.channel(`unread_messages:${chatId}`);
+    channel
+      .on(
+        "postgres_changes",
+        {
+          event: "UPDATE",
+          schema: "public",
+          table: "UserUnreadMessages",
+        },
+        (payload) => {
+          if (payload.new.chatId === chatId) {
+            if (payload.new.count > 0) setHasUnread(true);
+            else setHasUnread(false);          
+          }
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    }
+  }, [chatId, initialUnreadState])
+
+  
   return (
+    hasUnread ? 
     <Chip 
       color="warning" 
       variant="dot"
       radius="sm"
-      className="border-yellow-400 border-1.5 w-fit"
-      aria-label={`${unreadCount} messages non lus`}>
-      <span className="sm:hidden">
-        {unreadCount === 1 ? "1 nouveau" : `${unreadCount} nouveaux`}
-      </span>
-      <span className="hidden sm:inline">
-        {unreadCount === 1 ? "1 nouveau message" : `${unreadCount} nouveaux messages`}
-      </span>
-    </Chip>
+      className="border-yellow-400 border-1.5 w-fit">
+      <span>Nouveau(x) message(s)</span>
+    </Chip> : null
   )
 }
